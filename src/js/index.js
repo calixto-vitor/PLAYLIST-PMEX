f0aa3470028b9335253d149fb0e07ee178ccbb59
Vitor Calixto <calixtovitor33@gmail.com>
2023-01-30 14:56:15 -0300

    upgrade dropdown hover

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
